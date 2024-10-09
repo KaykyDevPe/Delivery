329ab9cf72473760da2e1f320836e4bf8cfe6d90
KaykyDevPe <kaykyr3123daniel@gmail.com>
2024-10-09 08:23:27 -0300

    02 - DELIVERY - 50:31

--- produto/urls.py
+++ produto/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("categoria/<int:id>", views.categoria, name="categoria"),
     path("produto/<int:id>", views.produto, name="produto"),
     path("add_carrinho", views.add_carrinho, name='add_carrinho'),
+    path("ver_carrinho", views.ver_carrinho, name="ver_carrinho")
 ]
